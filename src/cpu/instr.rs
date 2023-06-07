503434ccdca9c2ff2b75bceca6cca357b09e655d
BigflipBOT <filip.pyra@gmail.com>
2023-06-07 15:30:13 +0000

    resolved most of the problems pointed out in pr comments

--- src/cpu/instr.rs
+++ src/cpu/instr.rs
@@ -6,7 +6,6 @@ use std::collections::HashMap;
 bitflags! {
     #[derive(Debug, Clone, Copy, PartialEq, Eq)]
     pub struct Flags: u16 {
-        const CLEAR_FLAG = 0;
         const Z = (1 << 0); //zero
         const C = (1 << 1); //cary
         const N = (1 << 2); //negative
@@ -15,25 +14,6 @@ bitflags! {
     }
 }
 
-pub fn convert_to_bitflags(mut field: u16) -> Flags {
-    let mut temp_flag: Flags = Flags::CLEAR_FLAG;
-    if field & 0b1 == 1 { temp_flag |= Flags::Z; }
-    field = field >> 1;
-
-    if field & 0b1 == 1 { temp_flag |= Flags::C; }
-    field = field >> 1;
-
-    if field & 0b1 == 1 { temp_flag |= Flags::N; }
-    field = field >> 1;
-
-    if field & 0b1 == 1 { temp_flag |= Flags::O; }
-    field = field >> 1;
-
-    if field & 0b1 == 1 { temp_flag |= Flags::P; }
-    field = field >> 1;
-
-    return temp_flag;
-}
 
 #[derive(Debug)]
 pub struct Encoding {
@@ -61,27 +41,27 @@ impl Encoding {
 
 #[derive(PartialEq, Eq, Hash, Debug)]
 #[repr(u8)]
-enum Opcode { /* "//" <- means ready */
-    NOP = 0x0,//
-    MOV = 0x1,//
+enum Opcode { 
+    NOP = 0x0,
+    MOV = 0x1,
     LDD = 0x2,
-    ADD = 0x7,//
-    ADI = 0x8,//
-    SUB = 0xA,//
-    CMP = 0xC,//
-    CMI = 0xD,//
+    ADD = 0x7,
+    ADI = 0x8,
+    SUB = 0xA,
+    CMP = 0xC,
+    CMI = 0xD,
     JMP = 0xE,
-    JAL = 0xF,//
-    AND = 0x13,//
-    ORR = 0x14,//
-    XOR = 0x15,//
-    ANI = 0x16,//
-    ORI = 0x17,//
-    XOI = 0x18,//
-    SHL = 0x19,//
-    SHR = 0x1A,//
-    SLI = 0x23,//
-    SRI = 0x24,//
+    JAL = 0xF,
+    AND = 0x13,
+    ORR = 0x14,
+    XOR = 0x15,
+    ANI = 0x16,
+    ORI = 0x17,
+    XOI = 0x18,
+    SHL = 0x19,
+    SHR = 0x1A,
+    SLI = 0x23,
+    SRI = 0x24,
     DIV = 0x1D,
     MUL = 0x1C,
     MOD = 0x2C,
@@ -108,22 +88,25 @@ lazy_static! {
         m.insert(Opcode::MOV as u8, Operation {
             execute: |enc, cpu| {
                 cpu.state.reg[enc.rd as usize] = cpu.state.reg[enc.rs1 as usize];
+                
+                cpu.state.pc = cpu.state.pc+1;
             },
             repr: |enc| format!("mov r{}, r{}", enc.rd, enc.rs1),
         });
         m.insert(Opcode::LDD as u8, Operation {
             execute: |enc, cpu| {
                 cpu.state.reg[enc.rd as usize] = cpu.read(enc.imm, true);
+                
                 cpu.state.pc = cpu.state.pc+1;
             },
             repr: |enc| format!("ldd r{0}, {1}", enc.rd, enc.imm),
         });
-        m.insert(Opcode::ADD as u8, Operation { //changed implementation, because then gen_flag function would loose bits and wouldn't work.
+        m.insert(Opcode::ADD as u8, Operation {
             execute: |enc, cpu|{
                 let _out: u32 = cpu.state.reg[enc.rs1 as usize] as u32 + cpu.state.reg[enc.rs2 as usize] as u32; //this gives us acces to 17th bit
                 cpu.state.reg[enc.rd as usize] = _out as u16; // but the same value as before goes back to register
                 cpu.state.flags = gen_flag(false, cpu.state.reg[enc.rs1 as usize] as u32, cpu.state.reg[enc.rs2 as usize] as u32, _out); 
-                //happy genflag function here^
+                
                 cpu.state.pc = cpu.state.pc+1;
             },
             repr: |enc| format!("add r{0}, r{1}, r{2}", enc.rd, enc.rs1, enc.rs2),
@@ -166,7 +149,7 @@ lazy_static! {
 
             cpu.state.pc = cpu.state.pc+1;
             },
-            repr: |enc| format!("or r{0}, r{1}, r{2}", enc.rd, enc.rs1, enc.rs2),
+            repr: |enc| format!("orr r{0}, r{1}, r{2}", enc.rd, enc.rs1, enc.rs2),
         });
         m.insert(Opcode::XOR as u8, Operation {
             execute: |enc, cpu| {
@@ -180,7 +163,7 @@ lazy_static! {
         });
         m.insert(Opcode::ANI as u8, Operation {
             execute: |enc, cpu| {
-                let _out: u32 = cpu.state.reg[enc.rs1 as usize] as u32+ enc.imm as u32;
+                let _out: u32 = cpu.state.reg[enc.rs1 as usize] as u32 & enc.imm as u32;
                 cpu.state.reg[enc.rd as usize] = _out as u16;
                 cpu.state.flags = gen_flag(false, cpu.state.reg[enc.rs1 as usize] as u32, cpu.state.reg[enc.rs1 as usize] as u32, _out);
 
@@ -210,7 +193,7 @@ lazy_static! {
         });
         m.insert(Opcode::SHL as u8, Operation {
             execute: |enc, cpu| {
-                let _out: u32 = ((cpu.state.reg[enc.rs1 as usize] as u32) << (cpu.state.reg[enc.rs2 as usize] as u32)); //i have no fucking idea why this doesnt want to work,it's litteraly the same as other functions mijv6fi2j535f5r5gejihnegfboegoje9ef9nhef9e
+                let _out: u32 = ((cpu.state.reg[enc.rs1 as usize] as u32) << (cpu.state.reg[enc.rs2 as usize] as u32)); // necessary parentheses  
                 cpu.state.reg[enc.rd as usize] = _out as u16;
                 cpu.state.flags = gen_flag(false, cpu.state.reg[enc.rs1 as usize] as u32, cpu.state.reg[enc.rs2 as usize] as u32, _out);
 
@@ -230,8 +213,8 @@ lazy_static! {
         });
         m.insert(Opcode::SLI as u8, Operation {
             execute: |enc, cpu| {
-                let _out: u32 = ((cpu.state.reg[enc.rs1 as usize] as u32) << (enc.imm as u32));
-                cpu.state.reg[enc.rd as usize] = _out as u16; // again wtf????
+                let _out: u32 = ((cpu.state.reg[enc.rs1 as usize] as u32) << (enc.imm as u32));// necessary parentheses
+                cpu.state.reg[enc.rd as usize] = _out as u16; 
                 cpu.state.flags = gen_flag(false, cpu.state.reg[enc.rs1 as usize] as u32, enc.imm as u32, _out);
 
                 cpu.state.pc = cpu.state.pc+1;
@@ -287,35 +270,60 @@ lazy_static! {
                 cpu.state.reg[enc.rd as usize] = cpu.state.pc;
                 cpu.state.pc = enc.imm; 
             },
-            repr: |enc| format!("jal {}", enc.imm), //probably should be better
+            repr: |enc| format!("jal {} {}",enc.rd, enc.imm),
         });
         m.insert(Opcode::JMP as u8, Operation {
             execute: |enc, cpu| {
                 let jmp_code: u8 = ((enc.rs1 << 3) as u8) +  enc.rd as u8;
-                let cpu_flags: Flags = convert_to_bitflags(cpu.state.flags);
+                let cpu_flags: Flags = Flags::from_bits_truncate(cpu.state.flags);
+                let mut jump_condition_met: bool = false;
 
                 match jmp_code {
                     0x0 => cpu.state.pc = enc.imm,
-                    0x1 => if cpu_flags & Flags::C == Flags::C { cpu.state.pc = enc.imm},
-                    0x2 => if cpu_flags & Flags::Z == Flags::Z { cpu.state.pc = enc.imm},
-                    0x3 => if cpu_flags & Flags::N == Flags::N { cpu.state.pc = enc.imm},
-                    0x4 => if cpu_flags & (Flags::N | Flags::Z) != (Flags::N | Flags::Z) { cpu.state.pc = enc.imm },
-                    0x5 => if cpu_flags & (Flags::N | Flags::Z) == (Flags::N | Flags::Z) { cpu.state.pc = enc.imm },
-                    0x6 => if cpu_flags & Flags::N != Flags::N { cpu.state.pc = enc.imm},
-                    0x7 => if cpu_flags & Flags::Z != Flags::Z { cpu.state.pc = enc.imm},
-                    0x8 => if cpu_flags & Flags::O == Flags::O { cpu.state.pc = enc.imm},
-                    0x9 => if cpu_flags & Flags::P == Flags::P { cpu.state.pc = enc.imm},
-                    0xA => if cpu_flags & (Flags::C | Flags::Z) != (Flags::C | Flags::Z) { cpu.state.pc = enc.imm },
-                    0xB => if cpu_flags & Flags::C != Flags::C { cpu.state.pc = enc.imm},
-                    0xC => if cpu_flags & (Flags::C | Flags::Z) == (Flags::C | Flags::Z) { cpu.state.pc = enc.imm },
+                    0x1 => if cpu_flags.contains(Flags::C) { jump_condition_met = true },
+                    0x2 => if cpu_flags.contains(Flags::Z) { jump_condition_met = true },
+                    0x3 => if cpu_flags.contains(Flags::N) { jump_condition_met = true },
+                    0x4 => if !(cpu_flags.contains(Flags::N | Flags::Z)) { jump_condition_met = true },
+                    0x5 => if cpu_flags.contains(Flags::N | Flags::Z) { jump_condition_met = true },
+                    0x6 => if !(cpu_flags.contains(Flags::N)) { jump_condition_met = true },
+                    0x7 => if !(cpu_flags.contains(Flags::Z)) { jump_condition_met = true },
+                    0x8 => if cpu_flags.contains(Flags::O) { jump_condition_met = true },
+                    0x9 => if cpu_flags.contains(Flags::P) { jump_condition_met = true },
+                    0xA => if !(cpu_flags.contains(Flags::C | Flags::Z)) { jump_condition_met = true },
+                    0xB => if !(cpu_flags.contains(Flags::C)) { jump_condition_met = true },
+                    0xC => if !(cpu_flags.contains(Flags::C | Flags::Z)) { jump_condition_met = true },
                     _ => println!("jmp jump_code error! invalid instruction."),
                 }
 
-
-
+                if jump_condition_met {
+                    cpu.state.pc = enc.imm;
+                }
+                else {
+                    cpu.state.pc = cpu.state.pc+1;
+                }
 
             },
-            repr: |enc| { format!("jumped to {}", enc.imm) }, //looks ugly... TODO: do it better
+            repr: |enc| {
+                let jmp_code: u8 = ((enc.rs1 << 3) as u8) +  enc.rd as u8;
+                let jmp_code_str: &str;
+
+                match jmp_code {
+                    0x0 => jmp_code_str = "jmp",
+                    0x1 => jmp_code_str = "jca",
+                    0x2 => jmp_code_str = "jeq",
+                    0x3 => jmp_code_str = "jlt",
+                    0x4 => jmp_code_str = "jgt",
+                    0x5 => jmp_code_str = "jle",
+                    0x6 => jmp_code_str = "jge",
+                    0x7 => jmp_code_str = "jne",
+                    0x8 => jmp_code_str = "jovf",
+                    0x9 => jmp_code_str = "jpar",
+                    0xA => jmp_code_str = "jgtu",
+                    0xB => jmp_code_str = "jgeu",
+                    0xC => jmp_code_str = "jleu",
+                    _ => jmp_code_str = "jump error",
+                }
+                format!("{} to {}",jmp_code_str, enc.imm) }, //looks ugly... TODO: do it better
         });
         
 
@@ -334,21 +342,16 @@ pub fn execute(enc: &Encoding, cpu: &mut CPU) {
 }
 
 fn gen_flag(is_subtract: bool, var_1: u32, var_2: u32, mut var_out: u32) -> u16 {
-    let mut temp_flag = Flags::CLEAR_FLAG;
+    let mut temp_flag = Flags::empty();
     if extract(var_out, 15, 1) == 1 { temp_flag |= Flags::N; }
     
     if extract(var_out, 16, 1) == 1 { temp_flag |= Flags::C; }
 
-    if is_subtract {
-        if (extract(var_1, 15, 1) ^ extract(var_2, 15, 1) ^ 1) & (extract(var_1, 15, 1) ^ extract(var_out, 15, 1)) == 1 { temp_flag |= Flags::O; }
-    }
-    else {
-        if (extract(var_1, 15, 1) ^ extract(var_2, 15, 1) ^ 0) & (extract(var_1, 15, 1) ^ extract(var_out, 15, 1)) == 1 { temp_flag |= Flags::O; }
-    }
+    if (extract(var_1, 15, 1) ^ extract(var_2, 15, 1) ^ (is_subtract as u32)) & (extract(var_1, 15, 1) ^ extract(var_out, 15, 1)) == 1 { temp_flag |= Flags::O; }
+    
 
     if var_out as u16 == 0 { temp_flag |= Flags::Z; }
 
-    //bit magic
     var_out ^= var_out >> 8;
     var_out ^= var_out >> 4;
     var_out ^= var_out >> 2;
