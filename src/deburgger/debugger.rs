3a4f320220011f248578b37eb5a5a7be70019aa2
BigflipBOT <filip.pyra@gmail.com>
2023-07-09 22:18:56 +0000

    updated struct to use HashSet insted of Vec (speed) added minor improvments to menu added terminal colors :sunglasses:

--- src/deburgger/debugger.rs
+++ src/deburgger/debugger.rs
@@ -1,5 +1,6 @@
 use console::Term;
 use console::style;
+use std::collections::HashSet;
 
 use crate::cpu::cpu::CPU;
 
@@ -16,12 +17,12 @@ enum StatesD {
 pub struct Debugger<'a> {
     cpu: &'a mut CPU,
     state: StatesD,
-    watch_list_pc: Vec<u16>,    //program counter breakpoints   //probably will migrate to HashSet soon
+    watch_list_pc: HashSet<u16>,    //program counter breakpoints   //HashSet boss
 }
 
 impl Debugger<'_> {
     pub fn new(cpu: &mut CPU) -> Debugger {
-        Debugger { cpu: cpu, state: StatesD::Frozen, watch_list_pc: Vec::new(), }
+        Debugger { cpu: cpu, state: StatesD::Frozen, watch_list_pc: HashSet::new(), }
     }
 
     pub fn debuger_loop(&mut self) {
@@ -39,6 +40,7 @@ impl Debugger<'_> {
 
     fn catch_char(&self) -> char {
         let term: Term = Term::stdout();
+        //print!("some msg: ");
         let gówno = term.read_char();
         match gówno {
             Ok(ok) => return ok,
@@ -46,20 +48,24 @@ impl Debugger<'_> {
         }
     }
 
-    fn catch_string(&self) -> String {
+    fn catch_string(&self) -> String { // *niggy wiggy, i should probably add option for int extraction to watch for invalid input but i am too lazy for it rn. i count on that that users does not have severe skill issue.
+        //print!("input: ");
         let term: Term = Term::stdout();
-        let gówno = term.read_line();
-        print!{"input value: "}
-        match gówno {
+        
+        
+        let input_string = term.read_line();
+        match input_string {
             Ok(ok) => return ok,
             Err(error) => {println!{"error occured: {:?}", error} return String::from("0"); }
         }
     }
 
     fn check_watch(&mut self) -> bool {
-        for breakpoint_pc in &self.watch_list_pc {
-            if self.cpu.state.pc == *breakpoint_pc { return true };
+        if self.watch_list_pc.contains(&self.cpu.state.pc) { //piotr wawrzyniak
+            self.change_state_to(StatesD::Frozen);
         }
+
+        //other watches.
 
         return false
     }
@@ -67,22 +73,34 @@ impl Debugger<'_> {
     fn modify_menu(&mut self) {
         //watchlist print here
         if !self.watch_list_pc.is_empty() {
-            println!("{0: <10} |-----id-----|---value---|", "");
+            println!("{0: <10} |--{1:}--|", "", style("BrkpntPC").yellow() );    //change this to console crate or this other one
             
-            let mut counter: u32 = 0;
-            for breakpoint_pc in &self.watch_list_pc {
-                println!("{0: <10} | {1: <10} | {2: <9} |", "", counter, breakpoint_pc);
-                counter = counter + 1;
+            //i know how does it look. but trust me, this is the most sane way to do this.
+            let mut temp_hashset: HashSet<u16> = HashSet::with_capacity(self.watch_list_pc.capacity());
+            let temp_set = self.watch_list_pc.drain();
+            for breakpoint_pc in temp_set {
+                temp_hashset.insert(breakpoint_pc);
+                println!("{: <10} | {: <10} |", "", style(breakpoint_pc).on_magenta(), ); //drain sucks.
             }
-            
-            println!("{0: <10} |------------|-----------|", "");
+            self.watch_list_pc = temp_hashset;
+
+            println!("{0: <10} |____________|", "");
+            //println!("{0: <10} |--placehold-|", "");  //future mem breakpoints here
+            //println!("{0: <10} |____________|", "");
+            println!("");
         }
 
-        println!("a - add breakpoint, d - delete breakpoint, c - clear watchlist, b - back ");
+        println!("p - add Pc breakpoint, m - add Memory breakpoint, d - Delete breakpoint, c - Clear watchlist, b - Back ");
         //let mut x: u16;
         match self.catch_char() {
-            'a' => { self.watch_list_pc.push(self.catch_string().parse::<u16>().unwrap()); println!("") },// unsafe!!!!
-            'd' => { self.watch_list_pc.swap_remove(self.catch_string().parse::<usize>().unwrap()); println!("") }, //Vec.remove / Vec.swap_remove are wierd.
+            'p' => { self.watch_list_pc.insert(self.catch_string().parse::<u16>().unwrap()); println!("") },
+            'd' => { println!("p - pc breakpoint, m - mem breakpoint"); match self.catch_char() {
+                    'p' => { self.watch_list_pc.remove(&self.catch_string().parse::<u16>().unwrap()); println!(""); }, //niggy wiggy*
+                    'm' => todo!(),
+                     _ => println!("invalid type"),
+                }; println!("") },
+            'm' => todo!(),
+            //'d' => { self.watch_list_pc.remove(&self.catch_string().parse::<u16>().unwrap()); println!("") },   //sussy wussy. but it works. it's wierd that HashSets need & in wierd places but that's the price to pay for cool opts i guess. i am not going to tryhard and read whole docs on that.
             'c' => self.watch_list_pc.clear(),
             'b' => self.change_state_to(StatesD::Frozen),
             _ => println!("unknown/no command"),
@@ -97,23 +115,17 @@ impl Debugger<'_> {
             if irqc.borrow().active() {
                 self.cpu.sregs.add_interrupt(cpu::sreg::IRQF_EXT);
             }
-        */
+        */ //mom plz help me i am scared
     }
 
     fn choose_state(&mut self) {                
-        //user input handler here:
-        let term = Term::stdout();
-        let state_char: char = term.read_char().unwrap();
-                                //                    ^^^ koniec kariery
-
-        match state_char {
+        match self.catch_char() {
             's' => self.change_state_to(StatesD::Step), 
             'w' => self.change_state_to(StatesD::OnWatch),
             'c' => self.change_state_to(StatesD::Continuous),
             'm' => self.change_state_to(StatesD::Modify),
             _ => println!("unknown / no command"),
         }
-
     }
     
     fn change_state_to(&mut self, state_i: StatesD ) {
